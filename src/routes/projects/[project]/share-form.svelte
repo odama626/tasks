7f170aeb770bccd01b574e5f3b3527d087237973
Adam Sparks <adamsparks92@gmail.com>
2023-07-18 01:01:11 -0700

    load users on share form

--- src/routes/projects/[project]/share-form.svelte
+++ src/routes/projects/[project]/share-form.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	import { db } from "$lib/storage";
-	import { liveQuery } from "dexie";
+	import { db } from '$lib/storage';
+	import { liveQuery } from 'dexie';
 
 	export let projectId: string;
 	$: permissions = liveQuery(() => db.projects_users.where({ project: projectId }).toArray());
+	$: users = liveQuery(() => db.users.toCollection().toArray());
 </script>
 
 {#if $permissions}
-{#each $permissions as permission (permission.id)}
-	<div>
-		{permission.user}
-		{permission.access}
-	</div>
-{/each}
+	{#each $permissions as permission (permission.id)}
+		{@const user = $users.find((user) => user.id === permission.user)}
+		<div>
+			{user?.name || user?.username}
+			{permission.access}
+		</div>
+	{/each}
 {/if}
