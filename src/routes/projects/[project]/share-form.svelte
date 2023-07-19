9ea2c62c8366084420d5f23fffc94718662f1787
Adam Sparks <adamsparks92@gmail.com>
2023-07-18 23:30:12 -0700

    listbox

--- src/routes/projects/[project]/share-form.svelte
+++ src/routes/projects/[project]/share-form.svelte
@@ -1,18 +1,51 @@
 <script lang="ts">
 	import { db } from '$lib/storage';
+	import {
+		Listbox,
+		ListboxButton,
+		ListboxLabel,
+		ListboxOption,
+		ListboxOptions
+	} from '@rgossiaux/svelte-headlessui';
 	import { liveQuery } from 'dexie';
 
 	export let projectId: string;
 	$: permissions = liveQuery(() => db.projects_users.where({ project: projectId }).toArray());
 	$: users = liveQuery(() => db.users.toCollection().toArray());
+
+	let options = ['admin', 'editor', 'viewer'];
 </script>
 
 {#if $permissions}
-	{#each $permissions as permission (permission.id)}
-		{@const user = $users.find((user) => user.id === permission.user)}
-		<div>
-			{user?.name || user?.username}
-			{permission.access}
-		</div>
-	{/each}
+	<div class="permissions" on:click={(e) => e.stopPropagation()}>
+		{#each $permissions as permission (permission.id)}
+			{@const user = $users.find((user) => user.id === permission.user)}
+			<div class="name">
+				{user?.name || user?.username}
+			</div>
+			<div class="access">
+				<Listbox>
+					<ListboxButton>{permission.access}</ListboxButton>
+					<ListboxOptions>
+						{#each options as option}
+							<ListboxOption value={option}>{option}</ListboxOption>
+						{/each}
+					</ListboxOptions>
+				</Listbox>
+			</div>
+		{/each}
+	</div>
 {/if}
+
+<style lang="scss">
+	.permissions {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: var(--block-spacing);
+	}
+
+	.name,
+	.access {
+		align-self: center;
+	}
+</style>
