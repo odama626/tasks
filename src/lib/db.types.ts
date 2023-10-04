079726f78440fa4a2b511ba7b77b9df3a867549a
Adam Sparks <adamsparks92@gmail.com>
2023-10-04 00:46:24 -0700

    typegen

--- src/lib/db.types.ts
+++ src/lib/db.types.ts
@@ -43,6 +43,9 @@ export type DocAttachmentsRecord = {
 	doc?: RecordIdString
 	createdBy?: RecordIdString
 	deleted?: boolean
+	type?: string
+	size?: number
+	name?: string
 }
 
 export type DocRelationsRecord<TdocId = unknown, TparentId = unknown> = {
@@ -160,37 +163,8 @@ export interface CollectionSchemas {
   docs_users: Projectsusers;
   users_connections: Usersconnections;
   invites: Invites;
-  doc_attachments: Docattachments;
+  doc_attachments: Docs;
   doc_relations: Usersconnections;
-}
-
-export interface Docattachments {
-  id: string;
-  name: string;
-  type: string;
-  system: boolean;
-  schema: Schema7[];
-}
-
-export interface Schema7 {
-  id: string;
-  name: string;
-  type: string;
-  system: boolean;
-  required: boolean;
-  options: Options7;
-}
-
-export interface Options7 {
-  maxSelect?: number;
-  maxSize?: number;
-  mimeTypes?: any[];
-  thumbs?: any[];
-  protected?: boolean;
-  collectionId?: string;
-  cascadeDelete?: boolean;
-  minSelect?: any;
-  displayFields?: any[];
 }
 
 export interface Invites {
